72a0094dfa474b6bf1a04e6349307c8f1ba900d1
semsi94 <154756937+semsi94@users.noreply.github.com>
2024-12-26 09:36:08 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def start():
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
-    start()
+       uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
 
 # Dependency
 def get_db():
